90052301ec1254fcf61e148a1aeb99964ce0b3c9
Victor Basso <vitobasso@gmail.com>
2025-07-29 20:04:00 -0300

    filter table

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
-const relativePath = "../ai-scraper/output/20250701/statusinvest/data/ready/20250701T140910.csv"
+const statusinvestCsv = "../ai-scraper/output/20250701/statusinvest/data/ready/20250701T140910.csv"
 
 export async function GET() {
-    const filePath = path.join(process.cwd(), relativePath);
+    const filePath = path.join(process.cwd(), statusinvestCsv);
     const content = fs.readFileSync(filePath, 'utf8');
     return new NextResponse(content);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 "use client"
 
-import { useEffect, useState } from "react";
-import { Card } from "@/components/ui/card";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {useEffect, useState} from "react";
+import {Card} from "@/components/ui/card";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 
 export default function Home() {
     const [data, setData] = useState<string[][]>([]);
 
     useEffect(() => {
         fetch("/api/data")
-            .then((res) => res.text())
-            .then((csv) => {
-                const columnSeparator = ";"
-                const [headerLine, ...lines] = csv.trim().split("\n");
-                const headers = headerLine.split(columnSeparator);
-                const rows = lines.map((line) => line.split(columnSeparator));
-                setData([headers, ...rows]);
-            });
+            .then(res => res.text())
+            .then(csv => setData(filterCsv(csv)));
     }, []);
 
     if (data.length === 0) return <div className="p-4">Loading...</div>;
@@ -52,4 +46,57 @@ export default function Home() {
             </Table>
         </Card>
     );
+}
+
+const colsIncluded = [
+    "TICKER",
+    "LIQUIDEZ MEDIA DIARIA",
+    "P/L",
+    "P/VP",
+    "EV/EBIT",
+    "ROE",
+    "ROIC",
+    "MARG LIQUIDA",
+    "DIV LIQ / PATRI",
+    "LIQ CORRENTE",
+    "CAGR LUCROS 5 ANOS",
+    "DY",
+]
+
+const rowsIncluded = [
+    "ABCB4",
+    "BBAS3",
+    "BBSE3",
+    "CMIG4",
+    "CPFE3",
+    "CXSE3",
+    "FESA4",
+    "GGBR4",
+    "GOAU4",
+    "ISAE4",
+    "ITSA4",
+    "LEVE3",
+    "NEOE3",
+    "PETR4",
+    "PRIO3",
+    "RECV3",
+    "RENT3",
+    "ROMI3",
+    "TAEE11",
+    "UNIP6",
+    "VALE3",
+    "WEGE3",
+]
+
+function filterCsv(csv: string): string[][] {
+    const columnSeparator = ";"
+    const [headerLine, ...lines] = csv.trim().split("\n");
+    const allHeaders = headerLine.split(columnSeparator);
+    const selectedCols = colsIncluded.map(col => allHeaders.indexOf(col))
+    const headers = selectedCols.map(i => allHeaders[i]);
+    const rows = lines
+        .map(row => row.split(columnSeparator))
+        .map(row => selectedCols.map(i => row[i]))
+        .filter(row => rowsIncluded.includes(row[0]));
+    return [headers, ...rows];
 }
