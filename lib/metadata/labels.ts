7be429040cd2fef458806c5f0846ca77b1b0542b
Victor Basso <vitobasso@gmail.com>
2025-08-31 09:25:45 -0300

    fix yahoo_quote, derived.statusinvest. polish column-selector

--- lib/metadata/labels.ts
+++ lib/metadata/labels.ts
@@ -6,7 +6,9 @@ export type Labels = Record<string, Label>;
 
 export function makeLabelGetter(metadataKeys: Record<string, Label>,
                                 metadataPrefixes: Record<string, MetadataSource>): (path: string) => Label {
-    return (path: string) => metadataKeys[path] ?? getPrefixLabel(path, metadataPrefixes) ?? generateLabel(path)
+    const keys = {...metadataKeys, ...keyLabels}
+    const prefixes = {...metadataPrefixes, ...prefixLabels}
+    return (path: string) => keys[path] ?? getPrefixLabel(path, prefixes) ?? generateLabel(path)
 }
 
 function getPrefixLabel(path: string, metadata: Record<string, MetadataSource>): Label | undefined {
@@ -19,20 +21,18 @@ function getPrefixLabelByPrefix(path: string, metadata: Record<string, MetadataS
     return key ? metadata[key] : undefined;
 }
 
-const labels: Labels = {
+const keyLabels: Labels = {
     "ticker": {short: "Ação", long: "Código da Ação"},
 
     // from a source other than scraper
-    "quotes.latest": {short: "Hoje", long: "Cotação Hoje"},
+    "yahoo_quote.latest": {short: "Hoje", long: "Cotação Hoje"},
     "b3_position": {short: "B3, Posição", long: "Posição importada"},
     "b3_position.quantity": {short: "Qtd", long: "Quantidade"},
     "b3_position.average_price": {short: "PMed", long: "Preço Médio"},
 
     // derived from other keys
-    "derived.b3_position": {short: "Posição Calculada", long: "Posição calculada com a cotação atual"},
     "derived.b3_position.total_price": {short: "Total", long: "Valor Total"},
     "derived.b3_position.rendimento": {short: "Rend", long: "Rendimento em %"},
-    "derived.forecast": {short: "Yahoo Finance %", long: "Yahoo Finance e cotação atual"},
     "derived.forecast.min_pct": {short: "Min", long: "Previsão Mínima em %"},
     "derived.forecast.avg_pct": {short: "Méd", long: "Previsão Média em %"},
     "derived.forecast.max_pct": {short: "Máx", long: "Previsão Máxima em %"},
@@ -40,10 +40,19 @@ const labels: Labels = {
     "derived.statusinvest.ey": {short: "EY", long: "Earning Yield"},
 }
 
+const prefixLabels: Record<string, MetadataSource> = {
+
+    // from a source other than scraper
+    "yahoo_quote": {label: "Yahoo Finance API, tempo real", url: "https://github.com/gadicc/yahoo-finance2"},
+
+    // derived from other keys
+    "derived.b3_position": {label: "Posição Calculada", url: "Posição calculada com a cotação atual"},
+    "derived.forecast": {label: "Yahoo Finance %", url: "Yahoo Finance e cotação atual"},
+}
+
 function generateLabel(path: string): Label {
-    return labels[path] ?? {
-        short: titleize(getSuffix(path) ?? path),
-    };
+    let label = titleize(getSuffix(path) ?? path);
+    return { short: label, long: label };
 }
 
 function titleize(key: string) {
