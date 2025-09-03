cbaa3a7940da7ed2010ddf4bf78fdc401a72a2cb
Victor Basso <victor@veganhive.com>
2025-09-03 13:18:39 -0300

    colors tweaked

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -7,11 +7,26 @@ import {CheckIcon, MinusIcon} from "lucide-react"
 import {cn} from "@/lib/utils"
 
 function Checkbox({className, ...props}: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
-    const base = "peer group inline-flex items-center justify-center size-4 shrink-0 rounded-[4px] border border-input shadow-xs transition-shadow outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:bg-input/30"
-    const stateChecked = "data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary"
-    const stateIndeterminate = "data-[state=indeterminate]:bg-primary dark:data-[state=indeterminate]:bg-primary data-[state=indeterminate]:text-primary-foreground data-[state=indeterminate]:border-primary dark:data-[state=indeterminate]:border-primary"
-    const focusValidation = "focus-visible:ring-[3px] focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
-
+    const base = `
+  peer group inline-flex items-center justify-center size-4 shrink-0 rounded-[4px] border 
+  shadow-xs transition-shadow outline-none disabled:cursor-not-allowed disabled:opacity-50
+  bg-input dark:bg-input/30
+`
+    const stateChecked = `
+  data-[state=checked]:bg-primary
+  data-[state=checked]:text-primary-foreground
+  data-[state=checked]:border-primary
+`
+    const stateIndeterminate = `
+  data-[state=indeterminate]:bg-primary dark:data-[state=indeterminate]:bg-primary
+  data-[state=indeterminate]:text-primary-foreground
+  data-[state=indeterminate]:border-primary dark:data-[state=indeterminate]:border-primary
+`
+    const focusValidation = `
+  focus-visible:ring-[3px] focus-visible:border-ring focus-visible:ring-ring/50
+  aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40
+  aria-invalid:border-destructive
+`
     return (
         <CheckboxPrimitive.Root data-slot="checkbox" asChild {...props}>
             <div className={cn(base, stateChecked, stateIndeterminate, focusValidation, className)}>
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,21 +1,27 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-muted px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
+function Input({className, type, ...props}: React.ComponentProps<"input">) {
+    const base = `
+  flex h-9 w-full min-w-0 rounded-md bg-input border
+  px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none md:text-sm
+  dark:bg-input/30
+`
+    const placeholder = "placeholder:text-muted-foreground"
+    const selection = "selection:bg-primary selection:text-primary-foreground"
+    const file = "file:text-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium"
+    const disabled = "disabled:bg-muted disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+    const focus = "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]"
+    const invalid = "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive"
+
+
+    return (
+        <input type={type} data-slot="input"
+            className={cn(base, placeholder, selection, file, disabled, focus, invalid, className)}
+            {...props}
+        />
+    )
 }
 
-export { Input }
+export {Input}
