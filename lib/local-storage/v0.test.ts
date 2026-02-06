6e18f3833e6e874b3dceed438397442377d4e2ff
Victor Basso <vitobasso@gmail.com>
2026-02-06 16:08:01 -0300

    decouple migration v0 test from default data

--- lib/local-storage/v0.test.ts
+++ lib/local-storage/v0.test.ts
@@ -1,5 +1,7 @@
 import {describe, expect, test} from "@jest/globals";
 import {migrateV0ToV1} from "@/lib/local-storage/v0";
+import {ColView, RowView, ViewsAvailable} from "@/lib/views/views";
+import {mapValues, Rec} from "@/lib/utils/records";
 
 const rowsV0 = {
     "stock_br": [
@@ -172,59 +174,9 @@ const columnsV0 = {
     ]
 }
 
-const viewsAvailableV1 = {
+const viewsAvailableV1ExclDefaults = {
     "stock_br": {
         "rowViews": [
-            {
-                "name": "Radar",
-                "items": [
-                    "ITUB4",
-                    "BBDC4",
-                    "VALE3",
-                    "PETR4",
-                    "ABEV3",
-                    "BBAS3",
-                    "B3SA3",
-                    "WEGE3"
-                ]
-            },
-            {
-                "name": "Elétricas e Saneamento",
-                "items": [
-                    "CMIG4",
-                    "CPFE3",
-                    "EGIE3",
-                    "ENGI11",
-                    "EQTL3",
-                    "ISAE4",
-                    "NEOE3",
-                    "SAPR4",
-                    "SBSP3"
-                ]
-            },
-            {
-                "name": "Bancos e Seguradoras",
-                "items": [
-                    "BBAS3",
-                    "BBDC4",
-                    "ITSA4",
-                    "ITUB4",
-                    "BBSE3",
-                    "CXSE3"
-                ]
-            },
-            {
-                "name": "Comodities",
-                "items": [
-                    "PETR4",
-                    "VALE3",
-                    "GGBR4",
-                    "PRIO3",
-                    "RECV3",
-                    "SUZB3",
-                    "KLBN4"
-                ]
-            },
             {
                 "name": "Minhas Linhas",
                 "items": [
@@ -264,77 +216,6 @@ const viewsAvailableV1 = {
             }
         ],
         "colViews": [
-            {
-                "name": "Qualidade",
-                "items": [
-                    "b3.listagem.setor",
-                    "investidor10.cadastral.ano_de_fundacao",
-                    "investidor10.cadastral.ano_de_estreia_na_bolsa",
-                    "b3.listagem.segmento_de_negociacao",
-                    "investidor10.financeiro.patrimonio_liquido",
-                    "statusinvest.liquidez_media_diaria",
-                    "investidor10.financeiro.free_float",
-                    "investidor10.financeiro.tag_along",
-                    "statusinvest.div_liq_patri",
-                    "statusinvest.liq_corrente",
-                    "statusinvest.marg_liquida",
-                    "statusinvest.derived.ey",
-                    "statusinvest.roe",
-                    "statusinvest.roic",
-                    "simplywall.earnings_growth"
-                ]
-            },
-            {
-                "name": "Preço",
-                "items": [
-                    "b3.listagem.setor",
-                    "yahoo.quote.latest",
-                    "yahoo.derived.chart.1mo",
-                    "yahoo.chart.1y",
-                    "yahoo.chart.5y",
-                    "statusinvest.p_l",
-                    "statusinvest.p_vp",
-                    "statusinvest.p_ativos",
-                    "statusinvest.p_ebit"
-                ]
-            },
-            {
-                "name": "Analistas",
-                "items": [
-                    "b3.listagem.setor",
-                    "yahoo.recom.strong_buy",
-                    "yahoo.recom.buy",
-                    "yahoo.recom.hold",
-                    "yahoo.recom.sell",
-                    "yahoo.recom.strong_sell",
-                    "yahoo.derived.forecast.min_pct",
-                    "yahoo.derived.forecast.avg_pct",
-                    "yahoo.derived.forecast.max_pct"
-                ]
-            },
-            {
-                "name": "Rentabilidade",
-                "items": [
-                    "b3.derived.position.invested_value",
-                    "b3.derived.position.current_value",
-                    "b3.derived.position.price_variation",
-                    "b3.position.total_dividends",
-                    "b3.derived.position.total_value",
-                    "b3.derived.position.cumulative_return"
-                ]
-            },
-            {
-                "name": "Posição",
-                "items": [
-                    "b3.position.quantity",
-                    "b3.position.average_price",
-                    "b3.derived.position.price_variation",
-                    "yahoo.quote.latest",
-                    "yahoo.derived.chart.1mo",
-                    "yahoo.chart.1y",
-                    "yahoo.chart.5y"
-                ]
-            },
             {
                 "name": "Minhas Colunas",
                 "items": [
@@ -377,17 +258,6 @@ const viewsAvailableV1 = {
     "reit_br": {
         "rowViews": [
             {
-                "name": "Radar",
-                "items": [
-                    "KNCR11",
-                    "KNIP11",
-                    "XPML11",
-                    "HGLG11",
-                    "BTLG11",
-                    "KNRI11"
-                ]
-            },
-            {
                 "name": "Minhas Linhas",
                 "items": [
                     "ALZR11",
@@ -415,51 +285,6 @@ const viewsAvailableV1 = {
             }
         ],
         "colViews": [
-            {
-                "name": "Preço",
-                "items": [
-                    "yahoo.quote.latest",
-                    "yahoo.derived.chart.1mo",
-                    "yahoo.chart.1y",
-                    "yahoo.chart.5y",
-                    "fundamentus.p_vp"
-                ]
-            },
-            {
-                "name": "Fundamentos",
-                "items": [
-                    "fundamentus.segmento",
-                    "fundamentus.liquidez",
-                    "fundamentus.qtd_de_imoveis",
-                    "fundamentus.vacancia_media",
-                    "fundamentus.ffo_yield",
-                    "fundamentus.cap_rate",
-                    "fundamentus.dividend_yield"
-                ]
-            },
-            {
-                "name": "Rentabilidade",
-                "items": [
-                    "b3.derived.position.invested_value",
-                    "b3.derived.position.current_value",
-                    "b3.derived.position.price_variation",
-                    "b3.position.total_dividends",
-                    "b3.derived.position.total_value",
-                    "b3.derived.position.cumulative_return"
-                ]
-            },
-            {
-                "name": "Posição",
-                "items": [
-                    "b3.position.quantity",
-                    "b3.position.average_price",
-                    "b3.derived.position.price_variation",
-                    "yahoo.quote.latest",
-                    "yahoo.derived.chart.1mo",
-                    "yahoo.chart.1y",
-                    "yahoo.chart.5y"
-                ]
-            },
             {
                 "name": "Minhas Colunas",
                 "items": [
@@ -520,8 +345,23 @@ describe('migrateV0ToV1', () => {
     });
 
     test('creates v1 properties', () => {
-        expect(window.localStorage.getItem("viewsAvailable")).toBe(JSON.stringify(viewsAvailableV1));
+        // exclude default views to decouple the test from it
+        const viewsAvailableV1Result = window.localStorage.getItem("viewsAvailable");
+        const viewsAvailableV1ExclDefaultViews = removeDefaultViews(viewsAvailableV1Result);
+        expect(viewsAvailableV1ExclDefaultViews).toBe(JSON.stringify(viewsAvailableV1ExclDefaults));
         expect(window.localStorage.getItem("viewSelection")).toBe(JSON.stringify(viewSelectionV1));
     });
 
 });
+
+function removeDefaultViews(viewsAvailable: string | null) {
+    const result: Rec<ViewsAvailable> = viewsAvailable ? JSON.parse(viewsAvailable) : {};
+    const resultExclDefaultViews = mapValues(result, filterMigratedViews)
+    return JSON.stringify(resultExclDefaultViews);
+}
+
+function filterMigratedViews(views: ViewsAvailable) {
+    views.rowViews = views.rowViews.filter((v: RowView) => v.name === "Minhas Linhas");
+    views.colViews = views.colViews.filter((v: ColView) => v.name === "Minhas Colunas");
+    return views;
+}
